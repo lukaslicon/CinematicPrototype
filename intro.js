a06704c819528d73a529780239f27c00fc8cdf83
Lukas Licon <lukas@licons.com>
2023-05-28 18:59:22 -0700

    click to move

--- intro.js
+++ intro.js
@@ -8,5 +8,20 @@ class intro extends Phaser.Scene {
         this.player = this.physics.add.image(960, 590, 'player').setScale(1.5).setBounce(.6, .6);
         this.player.body.setCollideWorldBounds(true); 
 
+        
+        // Add a click (or touch) event listener to the scene
+        this.input.on('pointerdown', (pointer) => {
+            this.physics.moveTo(this.player, pointer.x, pointer.y, 300);
+                // Save destination coordinates
+                this.targetX = pointer.x;
+                this.targetY = pointer.y;
+        });
+    }
+
+    update() {
+        // If player is close to destination, stop the player
+        if (Phaser.Math.Distance.Between(this.player.x, this.player.y, this.targetX, this.targetY) < 10) {
+            this.player.body.setVelocity(0);
+        }
     }
 }
