ec7acff75dbc41bc3f52fc3b1ca68de5f5b7b801
Lukas Licon <lukas@licons.com>
2023-05-28 18:49:03 -0700

    formatting

--- load.js
+++ load.js
@@ -5,17 +5,7 @@ class load extends Phaser.Scene {
     preload ()
     {
         this.load.path = 'assets/images/';
-        this.load.image('summaryScreen', 'summaryscreen.png');
-        this.load.image('bob', 'bob.png');
-        this.load.image('boundary', 'boundary.png');
-        this.load.image('sideBoundary', 'sideBoundary.png');
-        this.load.image("timerBar", "timerBar.png");
-        this.load.image("timerBarBackground", "backgroundbar.png");
-        this.load.image("redtimerBar", "redbackgroundbar.png");
-        this.load.image("boarder", "boarder.png");
-        this.load.image("side", "sideBoarder.png");
-        this.load.image("background", "map.png");
-        this.load.image("reset", "reset.png");
+        this.load.image('background', 'map.png');
 
     }
     create()
