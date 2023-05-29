4cdeff98a49aabc79f1c8af0293996cf1152b21d
Lukas Licon <lukas@licons.com>
2023-05-28 18:48:59 -0700

    formatting

--- intro.js
+++ intro.js
@@ -4,8 +4,8 @@ class intro extends Phaser.Scene {
     }
 
     create() {
-        this.add.image(960,540 , 'summaryScreen');
-        this.add.text(725,850, "Click anywhere to begin.").setFontSize(30).setFill('#f0000f');
+        this.add.image(960,540 , 'background');
+        this.add
 
     }
 }
